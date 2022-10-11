6e59c5f886eae55b2a877d8ca8364b71bc4a492c
Greet Verduyn <greetverduyn@proximus.be>
2022-10-11 16:14:23 +0200

    basic lay-out

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.appContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.textInput} placeholder={'course Goals'}/>
+                <Button title={'add Goals'}/>
+            </View>
+            <View style={styles.goalsContainer}>
+                <Text>List of Goals</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        appContainer: {
+            flex: 1,
+            paddingTop: 50,
+            paddingHorizontal: 16,
+        },
+
+        inputContainer: {
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: 24,
+            borderBottomWidth: 1,
+            borderBottomColor: 'pink',
+        },
+
+        textInput: {
+            borderWidth: 1,
+            borderColor: '#cccccc',
+            width: '70%',
+            marginRight: 8,
+            padding: 8,
+        },
+    goalsContainer: {
+            flex: 5,
+    }
+    }
+);
